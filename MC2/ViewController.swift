e50574183ca7133f46f0b875b0470b8c30aee117
Vincent Alexander Christian <jvincentalexnder@gmail.com>
2020-05-10 20:28:59 +0700

    Initial Commit

---
+++ MC2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC2
+//
+//  Created by Vincent Alexander Christian on 10/05/20.
+//  Copyright © 2020 Vincent Alexander Christian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
